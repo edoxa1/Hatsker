b1fc5f9e6ff17d3f406918b87c72b266a28a92b5
eraidai58@gmail.com <eraidai58@gmail.com>
2020-01-23 21:42:19 +0600

    dz4

--- Hatsker/Form1.cs
+++ Hatsker/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Hatsker
 {
     public partial class Form1 : Form
@@ -29,9 +30,52 @@ namespace Hatsker
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string str = textBox1.Text;
-            this.BackColor = Color.Aqua;
-            MessageBox.Show($"Hello {str}","Caution");
+           
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        void ChangeForm(string col, string title, string width,string height)
+        {
+            this.BackColor = Color.FromName(col);
+            this.Text = title;
+
+            this.Width = Convert.ToInt32(width);
+            this.Height = Convert.ToInt32(height);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "" || textBox2.Text == "")
+            {
+                MessageBox.Show("Enter something in Text Boxes");
+            }
+            else
+            {
+                string BackgroundColor = textBox1.Text;
+                string FormTitle = textBox2.Text;
+                string wdth = textBox3.Text;
+                string hght = textBox4.Text;
+                ChangeForm(BackgroundColor, FormTitle, wdth, hght);
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
